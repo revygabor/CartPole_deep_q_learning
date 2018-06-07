14e389e8fbf69844db588f2a064c1283c52b56a8
revygabor <revy.gabor@gmail.com>
2018-06-07 10:36:21 +0200

    no target network

--- DQNAgent.py
+++ DQNAgent.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.models import save_model, load_model
 from keras.optimizers import Adam
 
@@ -13,17 +13,18 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.load_model = load_model
-        self.memory = deque()
-        self.gamma = 0.95  # Discount rate
+        self.memory = deque([], 500)
+        self.gamma = 0.95           # Discount rate
         if train:
-            self.epsilon = 1.0 # Explore rate
+            self.epsilon = 1.0      # Explore rate
         else:
             self.epsilon = 0
 
         self.epsilon_decay = 0.99
-        self.epsilon_min = 0.05
+        self.epsilon_min = 0.15
         self.learning_rate = 0.001
         self.model = self.build_model()
+
 
     def build_model(self):
         if self.load_model:
@@ -31,15 +32,19 @@ class DQNAgent:
 
         model = Sequential()
         model.add(Dense(64, input_dim=self.state_size, activation='relu'))
+        model.add(Dropout(rate=0.1))
         model.add(Dense(64, activation='relu'))
+        model.add(Dropout(rate=0.1))
         model.add(Dense(self.action_size, activation='linear'))
 
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 
         return model
 
+
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
+
 
     def act(self, state):
         if np.random.rand() < self.epsilon:
@@ -47,7 +52,13 @@ class DQNAgent:
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
 
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+
     def train(self, batch_size):
+
         minibatch = random.sample(self.memory, min(batch_size, len(self.memory)))
 
         for state, action, reward, next_state, done in minibatch:
@@ -55,14 +66,15 @@ class DQNAgent:
                 target = reward
 
             else:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+                target = reward + self.gamma * np.amax(self.predict(next_state)[0])
 
             target_f = self.model.predict(state)
             target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=2, verbose=0)
+            self.model.fit(state, target_f, epochs=10, verbose=0)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+
 
     def save_model(self):
         save_model(self.model, 'model.h5')
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import gym
 from DQNAgent import DQNAgent
 from numpy import reshape
+import math
 
 env = gym.make('CartPole-v0')
-env._max_episode_steps = 10000
+env._max_episode_steps = 500
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
+upper_bounds = [env.observation_space.high[0], 0.5, env.observation_space.high[2], math.radians(50)]
+lower_bounds = [env.observation_space.low[0], -0.5, env.observation_space.low[2], -math.radians(50)]
 
 load_model = True
 train = True
@@ -13,15 +16,23 @@ episodes = 5000
 
 agent = DQNAgent(state_size, action_size, load_model, train)
 
+
+def normalize(obs):
+    return [(obs[i] - lower_bounds[i]) / (upper_bounds[i] - lower_bounds[i]) for i in range(len(obs))]
+
+
+
 for e in range(episodes):
         state = env.reset()
+        next_state = normalize(state)
         state = reshape(state, [1, state_size])
 
-        for time_t in range(10000):
+        for time_t in range(env._max_episode_steps):
             env.render()
             action = agent.act(state)
 
             next_state, reward, done, _ = env.step(action)
+            next_state = normalize(next_state)
             next_state = reshape(next_state, [1, state_size])
 
             agent.remember(state, action, reward, next_state, done)
@@ -35,3 +46,4 @@ for e in range(episodes):
 
         if(e%100==0):
             agent.save_model()
+
